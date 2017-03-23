164f5ab98ae9201ce10957f44d8d5e527c3505aa
Ryan Brainard <brainard@heroku.com>
2017-03-23 20:20:28 +0900

    Basic KrDictXML parser

---
+++ parsers/krdict_xml.go
@@ -0,0 +1,50 @@
+package parsers
+
+import (
+	"github.com/ryanbrainard/jjogaegi/pkg"
+	"io"
+	"launchpad.net/xmlpath"
+	"log"
+)
+
+func KrDictXML(r io.Reader, items chan<- *pkg.Item, options map[string]string) {
+	root, err := xmlpath.Parse(r)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	iter := xmlpath.MustCompile("/LexicalResource/Lexicon/LexicalEntry").Iter(root)
+
+	for {
+		if !iter.Next() {
+			break
+		}
+
+		node := iter.Node()
+
+		items <- &pkg.Item{
+			Hangul: get(node, "Lemma/feat/@val"),
+			Hanja:  get(node, "feat[@att='origin']/@val"),
+			Def:    get(node, "Sense/feat[@att='definition']/@val"),
+			Examples: []pkg.Example{
+				{
+					Korean: get(node, "Sense/SenseExample/feat[@att='example']/@val"),
+				},
+			},
+		}
+	}
+
+	close(items)
+}
+
+func get(node *xmlpath.Node, xpath string) string {
+	path := xmlpath.MustCompile(xpath)
+
+	//log.Printf("xpath=%q exists=%t", xpath, path.Exists(node))
+
+	if value, ok := path.String(node); ok {
+		return value
+	}
+
+	return ""
+}
---
+++ parsers/krdict_xml_test.go
@@ -0,0 +1,40 @@
+package parsers
+
+import (
+	"github.com/ryanbrainard/jjogaegi/pkg"
+	"github.com/stretchr/testify/assert"
+	"os"
+	"testing"
+)
+
+var xmlTestItems = []*pkg.Item{
+	{
+		Hangul: "가입하다",
+		Hanja:  "加入하다",
+		Def:    "단체에 들어가거나 상품 및 서비스를 받기 위해 계약을 하다.",
+		Examples: []pkg.Example{
+			{
+				Korean: "동아리에 가입하다.",
+			},
+		},
+	},
+	{
+		Hangul: "갇히다",
+		Def:    "어떤 공간이나 상황에서 나가지 못하게 되다.",
+		Examples: []pkg.Example{
+			{
+				Korean: "갇힌 몸.",
+			},
+		},
+	},
+}
+
+func TestParseKrDictOppositesXML(t *testing.T) {
+	in, err := os.Open("fixtures/kr_dict_opposites.xml")
+	assert.Nil(t, err)
+
+	items := make(chan *pkg.Item, 100)
+	KrDictXML(in, items, map[string]string{})
+	assert.Equal(t, xmlTestItems[0], <-items)
+	assert.Equal(t, xmlTestItems[1], <-items)
+}
