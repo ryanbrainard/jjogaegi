60eeb371e1195e4b9c0a193a8180007832b98a83
Ryan Brainard <966764+ryanbrainard@users.noreply.github.com>
2022-01-09 09:28:31 +0900

    only add item fields if existing

--- parsers/naver_wordbook_json.go
+++ parsers/naver_wordbook_json.go
@@ -3,9 +3,9 @@ package parsers
 import (
 	"context"
 	"encoding/json"
-	"fmt"
 	"go.ryanbrainard.com/jjogaegi/pkg"
 	"io"
+	"log"
 )
 
 func ParseNaverWordbookJSON(ctx context.Context, r io.Reader, items chan<- *pkg.Item, options map[string]string) error {
@@ -17,7 +17,8 @@ func ParseNaverWordbookJSON(ctx context.Context, r io.Reader, items chan<- *pkg.
 
 	for _, mitem := range page.Data.MItems {
 		if mitem.ContentType != 0 {
-			return fmt.Errorf("unknown content type: %d", mitem.ContentType)
+			log.Printf("unknown content type=%d name=%s", mitem.ContentType, mitem.Name)
+			continue
 		}
 
 		content := &NaverWordbookItemContent{}
@@ -25,28 +26,42 @@ func ParseNaverWordbookJSON(ctx context.Context, r io.Reader, items chan<- *pkg.
 			return err
 		}
 
-		// using first element of each. TOOD: filter
-		member := content.Entry.Members[0]
-		prons := member.Prons[0]
-		mean := content.Entry.Means[0]
-		example := mean.Examples[0]
-		translation := example.Translations[0]
-
-		items <- &pkg.Item{
+		item := &pkg.Item{
 			ExternalID: content.Entry.EntryID,
-			Hangul:     member.EntryName,
-			Hanja:      member.OriginLanguage,
-			Def: pkg.Translation{
-				English: mean.ShowMean,
-			},
-			Pronunciation: prons.PronSymbol,
-			Examples: []pkg.Translation{
-				{
-					Korean:  example.ShowExample,
-					English: translation.ShowTranslation,
-				},
-			},
 		}
+
+		if len(content.Entry.Members) > 0 {
+			member := content.Entry.Members[0]
+
+			item.Hangul = member.EntryName
+			item.Hanja = member.OriginLanguage
+
+			if len(member.Prons) > 0 {
+				item.Pronunciation = member.Prons[0].PronSymbol
+			}
+		}
+
+		if len(content.Entry.Means) > 0 {
+			mean := content.Entry.Means[0]
+
+			item.Def = pkg.Translation{
+				English: mean.ShowMean,
+			}
+
+			for _, example := range mean.Examples {
+				var english string
+				if len(example.Translations) > 0 && example.Translations[0].Language == "en" {
+					english = example.Translations[0].ShowTranslation
+				}
+
+				item.Examples = append(item.Examples, pkg.Translation{
+					Korean:  example.ShowExample,
+					English: english,
+				})
+			}
+		}
+
+		items <- item
 	}
 
 	return nil
@@ -121,9 +136,9 @@ type NaverWordbookItemContent struct {
 			Language        string `json:"language"`
 			DescriptionJSON struct {
 			} `json:"description_json"`
-			MeanType  string `json:"mean_type"`
-			MeanLevel int    `json:"mean_level"`
-			MeanSeq   string `json:"mean_seq"`
+			MeanType  string      `json:"mean_type"`
+			MeanLevel int         `json:"mean_level"`
+			MeanSeq   interface{} `json:"mean_seq"`
 			Examples  []struct {
 				ExampleID       string `json:"example_id"`
 				ShowExample     string `json:"show_example"`
